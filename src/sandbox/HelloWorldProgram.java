142599efbd2cf78f0e8975e2525d6ce443625ff3
mukris <kristof.muki@mailbox.hu>
2014-02-26 11:28:31 +0100

    Kommentelés

--- src/sandbox/HelloWorldProgram.java
+++ src/sandbox/HelloWorldProgram.java
@@ -2,6 +2,10 @@ package sandbox;
 
 public class HelloWorldProgram {
 
+	/**
+	 * Main fv
+	 * @param args elsõ paraméter
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
