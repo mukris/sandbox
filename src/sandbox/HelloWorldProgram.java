f0bc933b2fe90557e94c344983736a751e580520
Erika <Erika@152.66.183.225>
2014-03-03 21:10:40 +0100

    Módosítottam, ki lehet próbálni.

--- src/sandbox/HelloWorldProgram.java
+++ src/sandbox/HelloWorldProgram.java
@@ -1,13 +1,21 @@
 package sandbox;
 
+import java.util.Scanner;
+
 public class HelloWorldProgram {
 
 	/**
 	 * Main fv
 	 * @param args elsõ paraméter
+	 * beolvas egy számot és annyiszor kiírja a szöveget
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello Kitty");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Írj egy számot 1-tõl 5-ig!");
+		int num = sc.nextInt();
+		for (int i=1; i<=num; i++) {
+			System.out.println("Hello World!");
+		}
 	}
 
 }
