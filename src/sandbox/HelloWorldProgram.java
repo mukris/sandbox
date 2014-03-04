44d8a13880f0204cacc48820d5140ac0b7199023
bauertomi <Duncan21>
2014-03-04 23:09:56 +0100

    Tomi voltam

--- src/sandbox/HelloWorldProgram.java
+++ src/sandbox/HelloWorldProgram.java
@@ -8,13 +8,15 @@ public class HelloWorldProgram {
 	 * Main fv
 	 * @param args elsõ paraméter
 	 * beolvas egy számot és annyiszor kiírja a szöveget
+	 * azért módosítom a kommentet, hogy ne szúrjak el semmit
+	 * de ha nem olvasod nem is veszed észre
 	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Írj egy számot 1-tõl 5-ig!");
 		int num = sc.nextInt();
 		for (int i=1; i<=num; i++) {
-			System.out.println("Hello World!");
+			System.out.println("Hello Kitty!");
 		}
 	}
 
